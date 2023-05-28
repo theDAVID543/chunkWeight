50174508765881151114a313aa1c5766a515e67b
theDAVID543 <theDAVID5432@gmail.com>
2023-05-28 21:42:41 +0800

    update

--- src/main/java/the/david/eventListener.java
+++ src/main/java/the/david/eventListener.java
@@ -111,12 +111,4 @@ public class eventListener implements Listener {
         Random random = new Random();
         return random.nextInt((upper - lower) + 1) + lower;
     }
-    public Location str2loc(String str){
-        String str2loc[]=str.split("\\:");
-        Location loc = new Location(Bukkit.getServer().getWorld(str2loc[0]),0,0,0);
-        loc.setX(Double.parseDouble(str2loc[1]));
-        loc.setY(Double.parseDouble(str2loc[2]));
-        loc.setZ(Double.parseDouble(str2loc[3]));
-        return loc;
-    }
 }
