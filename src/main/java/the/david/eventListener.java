efa660cdcf729f991c3b62bf93561a0995b31019
theDAVID543 <theDAVID5432@gmail.com>
2023-06-02 17:54:13 +0800

    improved save data

--- src/main/java/the/david/eventListener.java
+++ src/main/java/the/david/eventListener.java
@@ -22,6 +22,7 @@ public class eventListener implements Listener {
     private ArrayList<String> animals = new ArrayList<String>(
             Arrays.asList("ABSTRACTHORSE", "AXOLOTL", "BEE", "CAMEL", "CAT", "CHESTEDHORSE", "CHICKEN", "COW", "DONKEY", "FOX", "FROG", "GOAT", "HOGLIN", "HORSE", "LLAMA", "MULE", "MUSHROOMCOW", "OCELOT", "PANDA", "PARROT", "PIG", "POLARBEAR", "RABBIT", "SHEEP", "SKELETONHORSE", "SNIFFER", "STEERABLE", "STRIDER", "TAMEABLE", "TRADERLLAMA", "TURTLE", "WOLF", "ZOMBIEHORSE")
     );
+    private final Integer chunkLimit = 750;
 /*    @EventHandler
     public void onPickExpOrb(PlayerPickupExperienceEvent e){
         if(!Objects.equal(e.getExperienceOrb().getSourceEntityId(),null) && !Objects.equal(e.getExperienceOrb().getSpawnReason().toString(),"EXP_BOTTLE") && !animals.contains(uuidToType.get(e.getExperienceOrb().getSourceEntityId()).toString())){
@@ -41,12 +42,12 @@ public class eventListener implements Listener {
     @EventHandler
     public void onEntityDie(EntityDeathEvent e){
         if(!animals.contains(e.getEntityType().toString())){
-            if(!Objects.equal(entityConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity())),null) && getRandom(0,12500) >= entityConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity()))){
+            if(!Objects.equal(entityConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity())),null) && getRandom(0,chunkLimit) >= entityConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity()))){
                 e.setDroppedExp(0);
                 return;
             }
         }else{
-            if(!Objects.equal(animalConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity())),null) && getRandom(0,12500) >= animalConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity()))){
+            if(!Objects.equal(animalConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity())),null) && getRandom(0,chunkLimit) >= animalConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()),mobSpawnChunk.get(e.getEntity()))){
                 e.setDroppedExp(0);
                 return;
             }
@@ -54,13 +55,13 @@ public class eventListener implements Listener {
         if(!Objects.equal(e.getDroppedExp(),0)){
             if (!animals.contains(e.getEntityType().toString())) {
                 if (Objects.equal(entityConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity())), null)) {
-                    entityConfigReader.setConfig(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity()), 12500 - e.getDroppedExp());
+                    entityConfigReader.setConfig(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity()), chunkLimit - e.getDroppedExp());
                 } else {
                     entityConfigReader.setConfig(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity()), entityConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity())) - e.getDroppedExp());
                 }
             } else {
                 if (Objects.equal(animalConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity())), null)) {
-                    animalConfigReader.setConfig(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity()), 12500 - e.getDroppedExp());
+                    animalConfigReader.setConfig(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity()), chunkLimit - e.getDroppedExp());
                 } else {
                     animalConfigReader.setConfig(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity()), animalConfigReader.getChunkWeight(mobSpawnWorld.get(e.getEntity()), mobSpawnChunk.get(e.getEntity())) - e.getDroppedExp());
                 }
@@ -87,7 +88,7 @@ public class eventListener implements Listener {
     @EventHandler
     public void onBreed(EntityBreedEvent e){
         if(animals.contains(e.getEntityType().toString())){
-            if(!Objects.equal(animalConfigReader.getChunkWeight(e.getEntity().getWorld(), e.getEntity().getLocation().getChunk()),null) && getRandom(0,12500) >= animalConfigReader.getChunkWeight(e.getEntity().getWorld(), e.getEntity().getLocation().getChunk())) {
+            if(!Objects.equal(animalConfigReader.getChunkWeight(e.getEntity().getWorld(), e.getEntity().getLocation().getChunk()),null) && getRandom(0,chunkLimit) >= animalConfigReader.getChunkWeight(e.getEntity().getWorld(), e.getEntity().getLocation().getChunk())) {
                 e.setExperience(0);
             }
         }
@@ -95,12 +96,12 @@ public class eventListener implements Listener {
     @EventHandler
     public void onBlockDropExp(BlockExpEvent e){
         if(!Objects.equal(blockConfigReader.getChunkWeight(e.getBlock().getWorld(),e.getBlock().getLocation().getChunk()),null)){
-            if(getRandom(0,12500) >= blockConfigReader.getChunkWeight(e.getBlock().getWorld(),e.getBlock().getLocation().getChunk())){
+            if(getRandom(0,chunkLimit) >= blockConfigReader.getChunkWeight(e.getBlock().getWorld(),e.getBlock().getLocation().getChunk())){
                 e.setExpToDrop(0);
             }
         }
         if(Objects.equal(blockConfigReader.getChunkWeight(e.getBlock().getWorld(),e.getBlock().getChunk()),null)){
-            blockConfigReader.setConfig(e.getBlock().getWorld(),e.getBlock().getLocation().getChunk(), 12500 - e.getExpToDrop());
+            blockConfigReader.setConfig(e.getBlock().getWorld(),e.getBlock().getLocation().getChunk(), chunkLimit - e.getExpToDrop());
         }else{
             blockConfigReader.setConfig(e.getBlock().getWorld(),e.getBlock().getLocation().getChunk(), blockConfigReader.getChunkWeight(e.getBlock().getWorld(),e.getBlock().getChunk()) - e.getExpToDrop());
         }
